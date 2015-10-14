e4726e798d692f509a9745767bbfc0dc6bbd2774
joanjerezt <joanjerezt@gmail.com>
2015-10-14 13:32:03 +0200

    Caching implemented

--- service/src/main/java/edu/upc/eetac/dsa/beeter/dao/StingDAOImpl.java
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/dao/StingDAOImpl.java
@@ -1,8 +1,11 @@
 package edu.upc.eetac.dsa.beeter.dao;
 
+import edu.upc.eetac.dsa.beeter.BeeterMediaType;
 import edu.upc.eetac.dsa.beeter.entity.Sting;
 import edu.upc.eetac.dsa.beeter.entity.StingCollection;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 import java.sql.*;
 
 /**
@@ -73,6 +76,41 @@ public class StingDAOImpl implements StingDAO {
             if (connection != null) connection.close();
         }
         return sting;
+    }
+
+    @Path("/{id}")
+    @GET
+    @Produces(BeeterMediaType.BEETER_STING)
+    public Response getSting(@PathParam("id") String id, @Context Request request) {
+        // Create cache-control
+        CacheControl cacheControl = new CacheControl();
+        Sting sting = null;
+        StingDAO stingDAO = new StingDAOImpl();
+        try {
+            sting = stingDAO.getStingById(id);
+            if (sting == null)
+                throw new NotFoundException("Sting with id = " + id + " doesn't exist");
+
+            // Calculate the ETag on last modified date of user resource
+            EntityTag eTag = new EntityTag(Long.toString(sting.getLastModified()));
+
+            // Verify if it matched with etag available in http request
+            Response.ResponseBuilder rb = request.evaluatePreconditions(eTag);
+
+            // If ETag matches the rb will be non-null;
+            // Use the rb to return the response without any further processing
+            if (rb != null) {
+                return rb.cacheControl(cacheControl).tag(eTag).build();
+            }
+
+            // If rb is null then either it is first time request; or resource is
+            // modified
+            // Get the updated representation and return with Etag attached to it
+            rb = Response.ok(sting).cacheControl(cacheControl).tag(eTag);
+            return rb.build();
+        } catch (SQLException e) {
+            throw new InternalServerErrorException();
+        }
     }
 
     @Override
